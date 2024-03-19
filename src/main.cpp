a598d9c0aaeb197a129f8b57f3ae97e2e026a57e
Viktor Alexandersson <viktor.alexand@gmail.com>
2024-03-19 11:12:51 +0100

    Changes to CMake structure

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,8 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios) == -1) die("tcsetattr");
 }
-
 
 void enableRawMode() {
   if (tcgetattr(STDIN_FILENO, &origTermios) == -1) die("tcgetattr");
@@ -25,11 +24,11 @@ void enableRawMode() {
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
-  // c_cc == control characters, array of bytes that control various terminal settings
-  // VMIN == min number of bytes of input needed before read() can return
-  // VTIME == max amount of time to wait before read() returns
+  // c_cc == control characters, array of bytes that control various terminal
+  // settings VMIN == min number of bytes of input needed before read() can
+  // return VTIME == max amount of time to wait before read() returns
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
@@ -38,20 +37,18 @@ void enableRawMode() {
 int main() {
   enableRawMode();
 
-
   while (true) {
-    
     char c = '\0';
     int nread = read(STDIN_FILENO, &c, 1);
 
-    if ( nread == -1 && errno != EAGAIN ) die("read");
-    
+    if (nread == -1 && errno != EAGAIN) die("read");
+
     if (nread == 0) continue;
-    
+
     iscntrl(c) ? printf("%d\r\n", c) : printf("%d ('%c')\r\n", c, c);
-    if (c == 'q') break; 
+    if (c == 'q') break;
   }
-  disableRawMode();  
+  disableRawMode();
 
   return 0;
 }
